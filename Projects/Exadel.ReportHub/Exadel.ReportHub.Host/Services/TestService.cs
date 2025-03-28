c154a16a4d57584eca7a92bd2a1ce16b83d216f2
Dmitry925 <67834974+Dmitry925@users.noreply.github.com>
2025-03-28 14:43:52 +0300

    Updated TestService (#29)

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Services/TestService.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Services/TestService.cs
@@ -13,34 +13,33 @@ public class TestService(ISender sender) : BaseService
     }
 
     [HttpGet("{getError}")]
-    public async Task<IActionResult> GetTest([FromRoute] GetRequest request)
+    public async Task<IActionResult> GetTest([FromRoute] bool getError)
     {
-        var result = await sender.Send(request);
+        var result = await sender.Send(new GetRequest(getError));
 
         return FromResult(result);
     }
 
     [HttpPost]
-    public async Task<IActionResult> AddTest([FromBody] CreateRequest request)
+    public async Task<IActionResult> AddTest([FromBody] string name, [FromQuery] bool getError)
     {
-        var result = await sender.Send(request);
+        var result = await sender.Send(new CreateRequest(name, getError));
 
         return FromResult(result);
     }
 
     [HttpPut("{id:Guid}")]
-    public async Task<IActionResult> UpdateTest([FromRoute] Guid id, [FromBody] CreateRequest request)
+    public async Task<IActionResult> UpdateTest([FromRoute] Guid id, [FromBody] string name, [FromQuery] bool getError)
     {
-        var updateRequest = new UpdateRequest(id, request.Name, request.GetError);
-        var result = await sender.Send(updateRequest);
+        var result = await sender.Send(new UpdateRequest(id, name, getError));
 
         return FromResult(result);
     }
 
-    [HttpDelete("{id:guid}/{getError}")]
-    public async Task<IActionResult> DeleteTest([FromRoute] DeleteRequest request)
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> DeleteTest([FromRoute] Guid id, [FromQuery] bool getError)
     {
-        var result = await sender.Send(request);
+        var result = await sender.Send(new DeleteRequest(id, getError));
 
         return FromResult(result);
     }
