ff1333ff78aa590957af72611bf4eeaf336478fc
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-03-28 18:18:59 +0400

    Implemented password hash creation and getPasswordHash

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Common/PasswordHasher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exadel.ReportHub.Common;
+
+public class PasswordHasher
+{
+    private const int saltSize = 16;
+    private const int hashSize = 64;
+    private const int iterations = 100000;
+    private static readonly HashAlgorithmName hashAlgorithm = HashAlgorithmName.SHA512;
+
+    public static (string PasswordHash, string PasswordSalt) CreatePasswordHash(string password)
+    {
+        byte[] saltData = RandomNumberGenerator.GetBytes(saltSize);
+        string passwordSalt = Convert.ToBase64String(saltData);
+        byte[] hashedData = Rfc2898DeriveBytes.Pbkdf2(password,saltData,iterations,hashAlgorithm, hashSize);
+        string passwordHash = Convert.ToBase64String(hashedData);
+        return (passwordHash, passwordSalt);
+    }
+
+    public static string GetPasswordHash(string password, string passwordSalt)
+    {
+        byte[] saltData = Convert.FromBase64String(passwordSalt);
+        byte[] hashedData = Rfc2898DeriveBytes.Pbkdf2(password, saltData, iterations, hashAlgorithm, hashSize);
+
+        return Convert.ToBase64String(hashedData);
+    }
+}
--- Projects/Exadel.ReportHub/Exadel.ReportHub.Data/Models/User.cs
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Data/Models/User.cs
@@ -14,7 +14,9 @@ public class User : IDocument
 
     public string FullName { get; set; }
 
-    public string Password { get; set; }
+    public string PasswordHash { get; set; }
+
+    public string PasswordSalt { get; set; }
 
     public bool IsActive { get; set; } = true;
 }
