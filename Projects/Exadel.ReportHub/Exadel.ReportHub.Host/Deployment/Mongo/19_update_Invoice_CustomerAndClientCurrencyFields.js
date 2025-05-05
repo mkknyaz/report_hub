a9b3438e6e81ca075935d7d4bc2564f532313253
Giorgi Chekurishvili <giorgichekurishvili04@gmail.com>
2025-05-05 12:00:54 +0400

    Optimized MongoDb script to update invoices with customer and client currencies as requested (#186)

--- Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Deployment/Mongo/19_update_Invoice_CustomerAndClientCurrencyFields.js
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Host/Deployment/Mongo/19_update_Invoice_CustomerAndClientCurrencyFields.js
@@ -1,22 +1,57 @@
 ﻿const scriptName = "19_update_Invoice_CustomerAndClientCurrencyFields";
-const version = NumberInt(1);
+const version = NumberInt(2);
 
 if (db.MigrationHistory.findOne({ ScriptName: scriptName, Version: version })) {
     print(`${scriptName} v${version} is already applied`);;
     quit();
 }
 
-const clients = db.Client.find({}, { _id: 1, CurrencyId: 1, CurrencyCode: 1 }).toArray();
-
-const exchangeRates = db.ExchangeRate.find({}).toArray();
-
 const invoices = db.Invoice.find({}).toArray();
+
+const clientIds = [...new Set(invoices.map(invoice => invoice.ClientId))];
+const clients = db.Client.find({ _id: { $in: clientIds } }, { _id: 1, CurrencyId: 1, CurrencyCode: 1 }).toArray();
+
+const clientsDict = clients.reduce((key, client) => {
+    key[client._id] = {
+        CurrencyId: client.CurrencyId,
+        CurrencyCode: client.CurrencyCode
+    };
+    return key;
+}, {});
+
+const dates = db.Invoice.aggregate([
+    {
+        $group: {
+            _id: null,
+            minDate: { $min: "$IssueDate" },
+            maxDate: { $max: "$IssueDate" }
+        }
+    }
+]).toArray()[0];
+
+const exchangeRates = db.ExchangeRate.find({
+    RateDate: {
+        $gte: dates.minDate,
+        $lte: dates.maxDate
+    }
+}).toArray();
+
+const exchangeRatesDict = exchangeRates.reduce((key, rate) => {
+    if (!key[rate.Currency]) {
+        key[rate.Currency] = [];
+    }
+    key[rate.Currency].push(rate);
+    return key;
+}, {});
+
+db.ExchangeRate.createIndex({ RateDate: 1 });
+
 const updates = invoices.map(invoice => {
 
-    const client = clients.find(x => x._id.equals(invoice.ClientId));
+    const client = clientsDict[invoice.ClientId];
 
-    const clientCurrencyRates = exchangeRates.filter(x => x.Currency === client.CurrencyCode &&
-        new Date(x.RateDate) <= new Date(invoice.IssueDate));
+    const ratesForCurrency = exchangeRatesDict[client.CurrencyCode] || [];
+
 
     const update = {
         CustomerCurrencyAmount: invoice.Amount,
@@ -31,24 +66,22 @@ const updates = invoices.map(invoice => {
         update.ClientCurrencyAmount = invoice.Amount;
     }
     else {
+        const clientCurrencyRates = ratesForCurrency.filter(rate =>
+            new Date(rate.RateDate) <= new Date(invoice.IssueDate));
 
         if (clientCurrencyRates.length > 0) {
-            const closestRate = clientCurrencyRates.reduce((prev, curr) => {
-                const prevDiff = Math.abs(new Date(invoice.IssueDate).getTime() - new Date(prev.RateDate).getTime());
-                const currDiff = Math.abs(new Date(invoice.IssueDate).getTime() - new Date(curr.RateDate).getTime());
-                return currDiff < prevDiff ? curr : prev;
-            });
+            const maxDate = new Date(Math.max(...clientCurrencyRates.map(x =>
+                new Date(x.RateDate).getTime()
+            )));
 
-            let clientCurrencyAmount = invoice.Amount;
-            const clientCurrencyCode = client.CurrencyCode;
+            const closestRate = clientCurrencyRates.find(x =>
+                new Date(x.RateDate).getTime() === maxDate.getTime()
+            );
 
-            if (invoice.CurrencyCode !== clientCurrencyCode) {
-                const clientRate = closestRate.Rate;
-                clientCurrencyAmount = invoice.Amount * clientRate;
-            }
-
-            update.ClientCurrencyAmount = clientCurrencyAmount;
+            clientCurrencyAmount = invoice.Amount * closestRate.Rate;
+            update.ClientCurrencyAmount = NumberDecimal(clientCurrencyAmount.toFixed(4));
         }
+
     }
 
     return {
@@ -69,6 +102,8 @@ const updates = invoices.map(invoice => {
 if (updates.length > 0) {
     db.Invoice.bulkWrite(updates);
 }
+
+db.ExchangeRate.dropIndex({ RateDate: 1 });
 
 db.MigrationHistory.insertOne({
     ScriptName: scriptName,
