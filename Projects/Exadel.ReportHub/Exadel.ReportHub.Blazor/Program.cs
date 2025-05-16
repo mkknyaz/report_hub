8a9e1e697de5f5fc2b9001472f7f8d02f0987c40
Dmitry917 <rogowdmitriy917@gmail.com>
2025-05-16 20:25:14 +0300

    Added login page for blazor

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Blazor/Program.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics.CodeAnalysis;
+
+namespace Exadel.ReportHub.Blazor;
+
+[ExcludeFromCodeCoverage]
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+
+        await host.RunAsync();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+}
---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Blazor/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Exadel.ReportHub.Blazor.Components;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Exadel.ReportHub.Blazor;
+
+[ExcludeFromCodeCoverage]
+public class Startup()
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+        });
+    }
+}
