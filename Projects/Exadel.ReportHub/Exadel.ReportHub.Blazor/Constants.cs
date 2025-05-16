4d99688b099d7bd0e0ec122f575b3a9125bff9a4
Dmitry917 <rogowdmitriy917@gmail.com>
2025-05-16 22:15:02 +0300

    Added constants

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Blazor/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Exadel.ReportHub.Blazor;
+
+public static class Constants
+{
+    public static class Authentication
+    {
+        public const string ClientId = "report_hub_resource_owner";
+        public const string Scope = "report_hub_api";
+    }
+}
