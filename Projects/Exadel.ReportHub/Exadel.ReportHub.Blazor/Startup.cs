58050388adfc71207d20a6066182d3583b187337
Dmitry925 <67834974+Dmitry925@users.noreply.github.com>
2025-05-17 02:49:40 +0300

    Added login page for blazor (#226)

---
+++ Projects/Exadel.ReportHub/Exadel.ReportHub.Blazor/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Exadel.ReportHub.Blazor.Components;
+
+namespace Exadel.ReportHub.Blazor;
+
+[ExcludeFromCodeCoverage]
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+        });
+    }
+}
